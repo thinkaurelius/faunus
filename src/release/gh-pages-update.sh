6c7650a6ba8eb5a383e5b4613f8374affb8116dc
Dan LaRocque <dalaro@hopcount.org>
2014-04-21 20:46:26 -0400

    gh-pages-update.sh tweaks from 0.4.3 release

--- src/release/gh-pages-update.sh
+++ src/release/gh-pages-update.sh
@@ -3,45 +3,78 @@
 #
 # Update gh-pages during a release.
 #
-# Usage: gh-pages-update.sh release.properties
+# Usage:
 #
-# Clones the repo specified by scm.url in release.properties.
+#   gh-pages-update.sh release.properties
+#
+# OR
+#
+#   SCM_URL=/home/dalaro/tinkerelius/faunus-0.4.3 SCM_TAG=0.4.3 gh-pages-update.sh
+#
+# The first form reads SCM_URL and SCM_TAG from release.properties.
+# The second form provides these two variables to the script via the
+# environment.  Both of the variables must be set and must have
+# nonempty values.  If both the environment variables and a file
+# argument are provided, then the environment variables take
+# precedence.
+#
+# This script must be invoked from a git repository root.
+#
+# Regardless of how SCM_URL and SCM_TAG are provided, the script does
+# the following.
+#
+# * Create gh-pages branch in the local git repo if it doesn't
+#   already; it must already exist on the remote/origin end
+#
+# * Clone SCM_URL into a new repo in CLONE_DIR and cd there
+#
+# * Fetch and checkout gh-pages in the new clone
+#
+# * Copy contents of <original-repo-root>/doc/html into
+#   wikidoc/$SCM_TAG/ in the new clone
+#
+# * Copy Maven-generated javadocs from
+#   <original-repo-root>/target/apidocs into javadoc/$SCM_TAG/ in the
+#   new clone
+#
+# * Makes wikidoc/current/ mirror wikidoc/$SCM_TAG/ and makes
+#   javadoc/$SCM_TAG/ mirror javadoc/$SCM_TAG/ in the new clone
 # 
-# Checks out the gh-pages branch.
+# * Copies target/site-resources/index.html to base of gh-pages.
 #
-# Copies javadoc and wikidoc into (java|wiki)doc/scm.tag from
-# release.properties}/.
-#
-# Updates (java|wiki)doc/current/ to be symlinks to new docs.
-#
-# Copies target/site-resources/index.html to base of gh-pages.
-#
-# Does not commit or push anything.
-#
-
-[ -z "$1" ] && { echo "Usage: $0 path/to/release.properties"; exit -1; }
+# * Commits all changes in the new clone and pushes them to the
+#   original repository; does not push to github
 
 set -e
 set -u
 
 declare -r PAGE_BRANCH=gh-pages
-declare -r CLONE_DIR=${project.build.directory}/pages
-declare -r JAVADOC_DIR=${project.build.directory}/apidocs
-declare -r WIKIDOC_DIR=${project.basedir}/doc/html
-declare -r WIKI_INDEX=${project.build.directory}/release-resources/wikidoc-index.html
-declare -r MAIN_INDEX=${project.build.directory}/release-resources/index.html
+declare -r CLONE_DIR=/home/dalaro/tinkerelius/faunus-0.4.3/target/pages
+declare -r JAVADOC_DIR=/home/dalaro/tinkerelius/faunus-0.4.3/target/apidocs
+declare -r WIKIDOC_DIR=/home/dalaro/tinkerelius/faunus-0.4.3/doc/html
+declare -r WIKI_INDEX=/home/dalaro/tinkerelius/faunus-0.4.3/target/release-resources/wikidoc-index.html
+declare -r MAIN_INDEX=/home/dalaro/tinkerelius/faunus-0.4.3/target/release-resources/index.html
 
-# Search release.properties for the current release tag.
-declare -r SCM_TAG=`sed -rn 's|\\\\||g; s|^scm\.tag=||p' $1`
+if [ -z "${SCM_URL:-}" -o -z "${SCM_TAG:-}" ]; then
+    if [ -z "${1:-}" ]; then
+        echo "Usage: $0 path/to/release.properties"
+        exit -1
+    else
+        # Change directory to the folder containing release.properties,
+        # store the full path to that directory, then clone it to $CLONE_DIR.
+        pushd "`dirname $1`" >/dev/null
+        declare -r SCM_URL="`pwd`"
+        popd >/dev/null
+        echo Read SCM_URL from $1: $SCM_URL
 
-# Change directory to the folder containing release.properties,
-# store the full path to that directory, then clone it to $CLONE_DIR.
-pushd "`dirname $1`" >/dev/null
-declare -r SCM_URL="`pwd`"
-popd >/dev/null
-
-echo Set SCM_URL from $1: $SCM_URL
-echo Set SCM_TAG from $1: $SCM_TAG
+        # Search release.properties for the current release tag.
+        declare -r SCM_TAG=`sed -rn 's|\\\\||g; s|^scm\.tag=||p' $1`
+        echo Read SCM_TAG from $1: $SCM_TAG
+    fi
+else
+    echo Read SCM_URL from environment: $SCM_URL
+    echo Read SCM_TAG from environment: $SCM_TAG
+fi
 
 # If the gh-pages branch doesn't already exist, then create it.
 # The branch must exist for the sake of the clone we're about to make.
@@ -53,6 +86,7 @@ git clone "$SCM_URL" "$CLONE_DIR"
 cd "$CLONE_DIR"
 git fetch origin refs/remotes/origin/"$PAGE_BRANCH":refs/heads/"$PAGE_BRANCH"
 git checkout "$PAGE_BRANCH"
+git pull origin "$PAGE_BRANCH"
 
 echo Copying generated wikidoc to wikidoc/$SCM_TAG
 cd "$CLONE_DIR/wikidoc"
